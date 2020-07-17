5db1efaa4791191182c1b36cbad0ac680ab810b3
stinkymonkeyph <monkeydev.team@gmail.com>
2020-07-18 03:37:53 +0800

    support images does are not supported in hive-content-renderer

--- src/components/utilities/MarkdownViewer/index.js
+++ src/components/utilities/MarkdownViewer/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { DefaultRenderer } from "steem-content-renderer"
+import DefaultRenderer from "hive-content-renderer"
+import markdownLinkExtractor from 'markdown-link-extractor'
 import { createUseStyles } from 'react-jss'
 
 const renderer = new DefaultRenderer({
@@ -34,21 +35,41 @@ const useStyles = createUseStyles({
     '& a': {
       wordWrap: 'break-word',
       color: '#d32f2f',
+    },
+    '& p': {
+      fontSize: 15,
     }
   }
 })
 
+// prepare images that are currently not supported on hive-content-renderer
+const prepareImages = (content) => {
+  let body = content
+  const links = markdownLinkExtractor(content)
+
+  links.forEach((link) => {
+    if((link.includes('images.hive.blog') && link.includes('.webp'))) {
+      body = body.replace(link, `![](${link})`)
+    } else if (link.includes('dapplr-images') && !link.includes('images.hive.blog')) {
+      body = body.replace(link, `![](https://images.hive.blog/0x0/${link})`)
+    }
+  })
+
+  return body
+}
+
 
 const MarkdownViewer = (props) => {
   const classes = useStyles()
-  const { content } = props  
-  const safeHtmlStr = renderer.render(content)
+  let { content } = props
+  content = prepareImages(content)
+  content = renderer.render(content)
 
   return (
     <React.Fragment>
       <div
         className={classes.markdown}
-        dangerouslySetInnerHTML={{ __html: safeHtmlStr || '' }} 
+        dangerouslySetInnerHTML={{ __html: content || '' }} 
       />
     </React.Fragment>
   )
