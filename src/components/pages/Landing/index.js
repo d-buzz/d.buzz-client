0e803888a209ae99cecf6089a699c1d6d17ef5d6
stinkymonkeyph <monkeydev.team@gmail.com>
2020-08-18 23:34:07 +0800

    rename login to landing

---
+++ src/components/pages/Landing/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Trending } from 'components'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+  trendingWrapper: {
+    width: '100%',
+    minHeight: '100vh',
+    border: '1px solid #e6ecf0',
+  },
+  headerWrapper: {
+    width: '98%',
+    margin: '0 auto',
+  }
+})
+
+const Landing = () => {
+  const classes = useStyles()
+  return (
+    <React.Fragment>
+      <div className={classes.trendingWrapper}>
+        <div>
+          <h5 style={{ fontWeight: 'bold', marginLeft: 10, }}>Trending</h5>
+        </div>
+        <Trending unguardedLinks={true} />
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Landing
