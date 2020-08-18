0e4ebc592df0a6cf4869b947c6795c8b2e7d2ee4
stinkymonkeyph <monkeydev.team@gmail.com>
2020-08-19 01:27:18 +0800

    add search people component

---
+++ src/components/sections/SearchPeople/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchPeople = () => {
+  return (
+    <React.Fragment>
+      <h4>Hello There</h4>
+    </React.Fragment>
+  )
+}
+
+export default SearchPeople
