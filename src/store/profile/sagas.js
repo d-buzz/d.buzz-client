013676fc2e316da604c5000862c6da61b3e3eaf5
stinkymonkeyph <monkeydev.team@gmail.com>
2020-08-24 08:16:49 +0800

    fixed custom json notification read mark

--- src/store/profile/sagas.js
+++ src/store/profile/sagas.js
@@ -157,36 +157,42 @@ function* getFollowingRequest(payload, meta) {
 function* clearNotificationRequest(meta) {
   try {
     const user = yield select(state => state.auth.get('user'))
+    const notifications = yield select(state => state.polling.get('notifications'))
     const { username, useKeychain } = user
-    const operation = yield call(generateClearNotificationOperation, username)
+    const lastNotification = notifications[notifications.length-1]
+
+    const operation = yield call(generateClearNotificationOperation, username, lastNotification)
 
     let success = false
 
-    if(useKeychain) {
-      const result = yield call(broadcastKeychainOperation, username, operation)
-      success = result.success
-    } else {
-      let { login_data } = user
-      login_data = extractLoginData(login_data)
+    if(lastNotification.length !== 0) {
+      if(useKeychain) {
+        const result = yield call(broadcastKeychainOperation, username, operation)
+        success = result.success
+      } else {
+        let { login_data } = user
+        login_data = extractLoginData(login_data)
 
-      const wif = login_data[1]
-      const result = yield call(broadcastOperation, operation, [wif])
-      success = result.success
-    }
-
-    let old = yield select(state => state.polling.get('count'))
-
-    if(success) {
-      old = {
-        success: true,
-        lastread: '',
-        unread: 0,
+        const wif = login_data[1]
+        const result = yield call(broadcastOperation, operation, [wif])
+        success = result.success
       }
-    } else {
-      old.success = success
-    }
 
-    yield put(clearNotificationsSuccess(old, meta))
+      let old = yield select(state => state.polling.get('count'))
+
+      if(success) {
+        old = {
+          success: true,
+          lastread: '',
+          unread: 0,
+        }
+      } else {
+        old.success = success
+      }
+      yield put(clearNotificationsSuccess(old, meta))
+    } else {
+      yield put(clearNotificationsFailure('failed to clear notification', meta))
+    }
   } catch(error) {
     yield put(clearNotificationsFailure(error, meta))
   }
