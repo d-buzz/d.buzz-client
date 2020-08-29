a39db499f8bafd38a6e00176dd2607e7254cc8a6
Nelmin Jay Magan Anoc <monkeydev.team@gmail.com>
2020-08-30 01:54:09 +0800

    open new tab link preview fron post list (#91)
    * open new tab link preview fron post list
    
    * prevent propagation

--- src/components/common/PreviewLastLink/index.js
+++ src/components/common/PreviewLastLink/index.js
@@ -27,10 +27,16 @@ const PreviewLastLink = ({ className, content }) => {
     }
   }
 
+  const onClick = (e) => {
+    e.stopPropagation()
+    e.preventDefault()
+    window.open(url)
+  }
+
   return (
     <React.Fragment>
       {isValidUrl ? (
-        <div className={className}>
+        <div className={className} onClick={onClick}>
           <ReactTinyLink
             proxyUrl="http://167.71.204.127:8080"
             width="100%"
