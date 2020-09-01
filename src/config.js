e4d7e55d5b54c4955c1c871a81692512b2b86bcb
Nelmin Jay Magan Anoc <monkeydev.team@gmail.com>
2020-09-01 18:35:37 +0800

    transfer bucket (#118)

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ const config = {
   TAG: 'hive-193084',
   VERSION: process.env.REACT_APP_VERSION || '0.0.0',
   SEARCH_API: process.env.REACT_APP_SEARCH_API || 'http://localhost:3030/api/v1',
-  API_KEY: 'BagdcyAOVbPV4JLkbu62tQ==',
-  API_SECRET: 'piHDNxTXqQtNeg3sg51/+2MkGve8uuJGhJjLPhRTYCo=',
-  BUCKET: 'postnzt-team-bucket.storage.fleek.co',
+  API_KEY: 'ixc0LdVDyuWarUhdt9ix3w==',
+  API_SECRET: 'D4e7pwZphykKWsG0ElancXSzDmDEqImeC9yenZMMfVo=',
+  BUCKET: 'nathansenn-team-bucket.storage.fleek.co',
 }
 
 export default config
