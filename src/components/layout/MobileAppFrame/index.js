3712a7f2c6f9e367210cb66c0b1b08ab0e3ab937
stinkymonkeyph <monkeydev.team@gmail.com>
2020-09-10 14:07:01 +0800

    add mobile frame

---
+++ src/components/layout/MobileAppFrame/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileAppFrame = () => {
+  return (
+    <React.Fragment>
+      <h4 style={{ color: 'red' }}>I'm mobile</h4>
+    </React.Fragment>
+  )
+}
+
+export default MobileAppFrame
