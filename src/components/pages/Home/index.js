de183bef8bba2a613044c6dbd9eae34b91702780
stinkymonkeyph <monkeydev.team@gmail.com>
2020-09-21 14:56:05 +0800

    moved index clearing to infinite list

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -87,14 +87,10 @@ const Feeds = React.memo((props) => {
     // eslint-disable-next-line
   }, [last])
 
-  const clearIndexOnScroll = () => {
-    clearScrollIndex()
-  }
-
   return (
     <React.Fragment>
       <CreateBuzzForm />
-      <InfiniteList loading={loading} items={items} onScroll={loadMorePosts} clearIndex={clearIndexOnScroll} scrollToIndex={index} />
+      <InfiniteList loading={loading} items={items} onScroll={loadMorePosts} scrollToIndex={index} />
     </React.Fragment>
   )
 })
