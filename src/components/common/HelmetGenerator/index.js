bb059c9f52645d7dc854510b497cc8c2689c65fd
stinkymonkeyph <monkeydev.team@gmail.com>
2020-10-08 05:23:29 +0800

    fixed missing helmet component

---
+++ src/components/common/HelmetGenerator/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import stripHtml from 'string-strip-html'
+import { Helmet } from 'react-helmet'
+import { checkIfImage } from 'services/api'
+import removeMd from 'remove-markdown'
+import markdownLinkExtractor from 'markdown-link-extractor'
+
+const HelmetGenerator = (props) => {
+  const { content, user } = props
+  const [title, setTitle] = useState()
+  const [description, setDescription] = useState()
+  const [image, setImage] = useState()
+
+  useEffect(() => {
+    if(content && `${content}`.trim() !== '') {
+
+      const generateMeta = async () => {
+
+        const stripContent = stripHtml(removeMd(content))
+        let title = stripContent
+        let description = stripContent
+        const links = markdownLinkExtractor(content)
+
+        if(`${title}`.length > 80) {
+          title = `${title.substr(0, 80)} ...`
+        }
+
+        title = `${title} | D.Buzz`
+
+        if(description > 120) {
+          description = `${description.substr(0, 120)} ...`
+        }
+
+        description = `${description} | by ${user}`
+        const avatarLink = `https://images.hive.blog/u/${user}/avatar/large`
+
+        setTitle(title)
+        setDescription(description)
+
+        if(links.length !== 0) {
+          const result = await checkIfImage(links)
+          const { hasImage, imageUrl } = result
+          if(hasImage) {
+            setImage(`https://images.hive.blog/0x0/${imageUrl}`)
+          } else {
+            setImage(avatarLink)
+          }
+        } else {
+          setImage(avatarLink)
+        }
+      }
+
+      generateMeta()
+
+    }
+    // eslint-disable-next-line
+  }, [content])
+
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:og:image" content={image} />
+      </Helmet>
+    </React.Fragment>
+  )
+}
+
+export default HelmetGenerator
