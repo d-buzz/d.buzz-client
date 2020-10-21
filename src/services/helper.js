1461f307988c9a8c6e71c614f1a11df00a521603
stinkymonkeyph <monkeydev.team@gmail.com>
2020-10-21 23:50:40 +0800

    add secure session generator and reader

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,4 +1,8 @@
 import { useState, useEffect } from 'react'
+import uuid from 'uuid-random'
+import { encrypt, decrypt } from 'caesar-shift'
+import CryptoJS  from 'crypto-js'
+import sha256 from 'crypto-js/sha256'
 import diff_match_patch from 'diff-match-patch'
 const dmp = new diff_match_patch()
 
@@ -166,4 +170,62 @@ export function hasCompatibleKeychain() {
     window.hive_keychain.requestBroadcast &&
     window.hive_keychain.requestSignedCall
   )
+}
+
+const randomizer = (max, min) => {
+  return Math.random() * (max - min) + min
+}
+
+const keygen = (index) => {
+  let key = 5 + index
+  if(key > 28) {
+    key = key - 5
+  }
+
+  key = Math.ceil(key)
+
+  return key
+}
+
+export const readSession = (session) => {
+  const { id, token } = session
+  const idkey = `${id}x0`
+
+  let sessionDec = CryptoJS.AES.decrypt(token, idkey)
+  sessionDec = JSON.parse(sessionDec.toString(CryptoJS.enc.Utf8))
+
+  const { index, uid, data } = sessionDec
+  const uuid = decrypt(index, uid)
+  const hash = sha256(uuid).toString()
+
+  let dataDec = CryptoJS.AES.decrypt(data, hash)
+  dataDec = JSON.parse(dataDec.toString(CryptoJS.enc.Utf8))
+
+  return dataDec
+}
+
+export const generateSession = (obj) => {
+  const date = new Date()
+  const lowerlimit = randomizer(1, 12)
+  const upperlimit = randomizer(13, 28)
+  const index = randomizer(lowerlimit, upperlimit)
+
+  const uid = uuid()
+  const key = keygen(index)
+
+  const caesar = encrypt(key, uid)
+  const hash = sha256(uid).toString()
+
+  const data = CryptoJS.AES.encrypt(JSON.stringify(obj), hash).toString()
+  const id = Math.ceil(date.getTime())
+
+  let token = {
+    index: key,
+    uid: caesar,
+    data,
+  }
+
+  token = CryptoJS.AES.encrypt(JSON.stringify(token), `${id}x0`).toString()
+
+  return { id, token }
 }
