e4bb0a1277575055ac105227e27e8e12e7dbeee0
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2020-11-24 20:08:57 +0800

    added whitelist of the vimm embed

--- src/components/common/LinkPreview/index.js
+++ src/components/common/LinkPreview/index.js
@@ -82,13 +82,14 @@ const LinkPreview = (props) => {
           && !link.includes('pbs.twimg.com')
           && !link.includes('ipfs.io')
           && !link.includes('3speak.co')
-          && !link.includes('www.vimm.tv')
+          && !link.includes('www.vimm.tv/view')
           && link.match( /^[https][http]/)
           && !link.match(/(?:https?:\/\/(?:(?:twitter\.com\/(.*?)\/status\/(.*))))/i)
           && !link.match(/(?:https?:\/\/(?:(?:twitter\.com\/(.*?)\/status\/(.*)?=(.*))))/i)
           && !link.match(/(?:https?:\/\/(?:(?:3speak\.online\/watch\?v=(.*))))/i)
           && !link.match(/(?:https?:\/\/(?:(?:3speak\.co\/watch\?v=(.*))))/i)
           && !link.match(/(?:https?:\/\/(?:(?:www\.vimm\.tv\/(.*?)\/embed)))/i)
+          && !link.match(/(?:https?:\/\/(?:(?:www\.vimm\.tv\/(.*?))))/i)
           && !link.match(/\.(jpeg|jpg|gif|png|pdf|JPG)$/)) {
         url = link
         isValidUrl = true
