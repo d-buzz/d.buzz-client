1a3c22b2e72223f21342c1300c61b1b592362919
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2020-12-10 13:26:59 +0800

    addded image api config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const config = {
   VERSION: process.env.REACT_APP_VERSION || '0.0.0',
   SEARCH_API: process.env.REACT_APP_SEARCH_API || 'http://localhost:3030/api/v1',
   SCRAPE_API: process.env.REACT_APP_SCRAPE_API,
+  IMAGE_API:  process.env.REACT_APP_IMAGE_API || 'http://localhost:3040/api/v1',
   API_KEY: 'ixc0LdVDyuWarUhdt9ix3w==',
   API_SECRET: 'D4e7pwZphykKWsG0ElancXSzDmDEqImeC9yenZMMfVo=',
   BUCKET: 'nathansenn-team-bucket.storage.fleek.co',
