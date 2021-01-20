e8fbe11ae90e706aa56abb59217420d981be6387
stinkymonkeyph <monkeydev.team@gmail.com>
2021-01-20 23:12:51 +0800

    destructure global property

--- src/store/profile/sagas.js
+++ src/store/profile/sagas.js
@@ -55,9 +55,10 @@ function* getProfileRequest(payload, meta) {
     const profile = yield call(fetchSingleProfile, username)
     const account = yield call(fetchAccounts, username)
     const { vesting_shares, to_withdraw, withdrawn, delegated_vesting_shares } = account[0]
-    const delagated = parseFloat(parseFloat(props.total_vesting_fund_hive) * (parseFloat(delegated_vesting_shares) / parseFloat(props.total_vesting_shares)),6)
+    const { total_vesting_fund_hive, total_vesting_shares } = props
+    const delagated = parseFloat(parseFloat(total_vesting_fund_hive) * (parseFloat(delegated_vesting_shares) / parseFloat(total_vesting_shares)),6)
     const avail = parseFloat(vesting_shares) - (parseFloat(to_withdraw) - parseFloat(withdrawn)) / 1e6 - parseFloat(delegated_vesting_shares)
-    const vestHive = parseFloat(parseFloat(props.total_vesting_fund_hive) * (parseFloat(avail) / parseFloat(props.total_vesting_shares)),6)
+    const vestHive = parseFloat(parseFloat(total_vesting_fund_hive) * (parseFloat(avail) / parseFloat(total_vesting_shares)),6)
 
     profile.hivepower = vestHive.toFixed(2)
     profile.delegated = delagated.toFixed(2)
