d5ba37f76eb067ed27e8cb3470d65a9a2b7334ee
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-01-23 05:55:52 +0800

    refactored disclaimer page and applied mobile view capability

--- src/components/pages/Disclaimer/index.js
+++ src/components/pages/Disclaimer/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Scrollspy from 'react-scrollspy'
 import { HashLink } from 'react-router-hash-link'
 import { createUseStyles } from 'react-jss'
-import { Sticky } from 'react-sticky'
+import { Sticky, StickyContainer } from 'react-sticky'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Container } from '@material-ui/core'
 
 const useStyles = createUseStyles(theme => ({
   wrapper: {
@@ -24,6 +25,10 @@ const useStyles = createUseStyles(theme => ({
   hero: {
     paddingTop: 100,
     textAlign: 'justify',
+  },
+  outsideWrapper: {
+    paddingTop: 50,
+    ...theme.font,
   },
   sideWrapper: {
     width: '100%', 
@@ -57,112 +62,116 @@ const Disclaimer = () => {
   const classes = useStyles()
   return (
     <React.Fragment>
-      <div className={classes.wrapper}>
-        <Row>
-          <Col xs={4}>
-            <Sticky>
-              {({ style }) => (
-                <div style={{...style}}>
-                  <div className={classes.sideWrapper}>
-                    <Scrollspy items={[
-                      'section-1', 
-                      'section-2', 
-                      'section-3', 
-                      'section-4', 
-                      'section-5',
-                    ]} currentClassName={classes.currentLink}>
-                      <li><HashLink to="#section-1">Website Disclaimer</HashLink></li>
-                      <li><HashLink to="#section-2">External Links Disclaimer</HashLink></li>
-                      <li><HashLink to="#section-3">Professional Disclaimer</HashLink></li>
-                      <li><HashLink to="#section-4">Affiliates Disclaimer</HashLink></li>
-                      <li><HashLink to="#section-5">Testimonials Disclaimer</HashLink></li>
-                    </Scrollspy>
-                  </div>
+      <Container maxWidth="md" fluid>
+        <div className={classes.wrapper}>
+          <StickyContainer>
+            <Row>
+              <Col xs={5} className="d-none d-md-block">
+                <Sticky>
+                  {({ style }) => (
+                    <div style={{...style}}>
+                      <div className={classes.sideWrapper}>
+                        <Scrollspy items={[
+                          'section-1', 
+                          'section-2', 
+                          'section-3', 
+                          'section-4', 
+                          'section-5',
+                        ]} currentClassName={classes.currentLink}>
+                          <li><HashLink to="#section-1">Website Disclaimer</HashLink></li>
+                          <li><HashLink to="#section-2">External Links Disclaimer</HashLink></li>
+                          <li><HashLink to="#section-3">Professional Disclaimer</HashLink></li>
+                          <li><HashLink to="#section-4">Affiliates Disclaimer</HashLink></li>
+                          <li><HashLink to="#section-5">Testimonials Disclaimer</HashLink></li>
+                        </Scrollspy>
+                      </div>
+                    </div>
+                  )}
+                  
+                </Sticky>
+              </Col>
+              <Col>
+                <div className={classes.innerWrapper}>
+                  <section id="section-1">
+                    <div className={classes.hero}>
+                      <div >
+                        <h3>Website Disclaimer</h3>
+                      </div>
+                      <hr />
+                      <div >
+                        <p>
+                          The information provided by D.Buzz (“we,” “us” or “our”) on <a href="https://d.buzz">https://d.buzz</a> (the “Site”) and our mobile application is for general informational purposes only. All information on the Site and our mobile application is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability or completeness of any information on the Site or our mobile application. UNDER NO CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THE SITE OR OUR MOBILE APPLICATION OR RELIANCE ON ANY INFORMATION PROVIDED ON THE SITE AND OUR MOBILE APPLICATION. YOUR USE OF THE SITE AND OUR MOBILE APPLICATION AND YOUR RELIANCE ON ANY INFORMATION ON THE SITE AND OUR MOBILE APPLICATION IS SOLELY AT YOUR OWN RISK.
+                        </p>
+                      </div>
+                    </div>
+                  </section>
+                  <section id="section-2">
+                    <div className={classes.hero}>
+                      <div >
+                        <h3>External Links Disclaimer</h3>
+                      </div>
+                      <hr />
+                      <div >
+                        <p>
+                          The Site and our mobile application may contain (or you may be sent through the Site or our mobile application) links to other websites or content belonging to or originating from third parties or links to websites and features in banners or other advertising. Such external links are not investigated, monitored, or checked for accuracy, adequacy, validity, reliability, availability or completeness by us. WE DO NOT WARRANT, ENDORSE, GUARANTEE, OR ASSUME RESPONSIBILITY FOR THE ACCURACY OR RELIABILITY OF ANY INFORMATION OFFERED BY THIRD-PARTY WEBSITES LINKED THROUGH THE SITE OR ANY WEBSITE OR FEATURE LINKED IN ANY BANNER OR OTHER ADVERTISING. WE WILL NOT BE A PARTY TO OR IN ANY WAY BE RESPONSIBLE FOR MONITORING ANY TRANSACTION BETWEEN YOU AND THIRD-PARTY PROVIDERS OF PRODUCTS OR SERVICES.
+                        </p>
+                      </div>
+                    </div>
+                  </section>
+                  <section id="section-3">
+                    <div className={classes.hero}>
+                      <div >
+                        <h3>Professional Disclaimer</h3>
+                      </div>
+                      <hr />
+                      <div >
+                        <p>
+                          The Site cannot and does not contain cryptocurrency advice. The cryptocurrency information is provided for general informational and educational purposes only and is not a substitute for professional advice. Accordingly, before taking any actions based upon such information, we encourage you to consult with the appropriate professionals. We do not provide any kind of cryptocurrency advice. THE USE OR RELIANCE OF ANY INFORMATION CONTAINED ON THIS SITE OR OUR MOBILE APPLICATION IS SOLELY AT YOUR OWN RISK.
+                        </p>
+                      </div>
+                    </div>
+                  </section>
+                  <section id="section-4">
+                    <div className={classes.hero}>
+                      <div >
+                        <h3>Affiliates Disclaimer</h3>
+                      </div>
+                      <hr />
+                      <div >
+                        <p>
+                        You may not access or use the Site for any purpose other than that for which we make the Site available. The Site may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us.
+                        </p>
+                      </div>
+                    </div>
+                  </section>
+                  <section id="section-5">
+                    <div className={classes.hero}>
+                      <div >
+                        <h3>Testimonials Disclaimer</h3>
+                      </div>
+                      <hr />
+                      <div style={{paddingBottom: 300}}>
+                        <p>
+                          The Site may contain testimonials by users of our products and/or services. These testimonials reflect the real-life experiences and opinions of such users. However, the experiences are personal to those particular users, and may not necessarily be representative of all users of our products and/or services. We do not claim, and you should not assume, that all users will have the same experiences. YOUR INDIVIDUAL RESULTS MAY VARY. 
+                        </p>
+                        <p>
+                          The testimonials on the Site are submitted in various forms such as text, audio and/or video, and are reviewed by us before being posted. They are eddited to fit the size and lenght we have space for.
+                        </p>
+                        <p>
+                          The views and opinions contained in the testimonials belong solely to the individual user and do not reflect our views and opinions. We are not affiliated with users who provide testimonials, and users are not paid or otherwise compensated for their testimonials.
+                        </p>
+                        <p>
+                          The testimonials on the Site are not intended, nor should they be construed, as claims that our products and/or services can be used to diagnose, treat, mitigate, cure, prevent or otherwise be used for any disease or medical condition. No testimonials have been clinically proven or evaluated.
+                        </p>
+                      </div>
+                    </div>
+                  </section>
                 </div>
-              )}
-              
-            </Sticky>
-          </Col>
-          <Col xs={8}>
-            <div className={classes.innerWrapper}>
-              <section id="section-1">
-                <div className={classes.hero}>
-                  <div >
-                    <h3>Website Disclaimer</h3>
-                  </div>
-                  <hr />
-                  <div >
-                    <p>
-                      The information provided by D.Buzz (“we,” “us” or “our”) on <a href="https://d.buzz">https://d.buzz</a> (the “Site”) and our mobile application is for general informational purposes only. All information on the Site and our mobile application is provided in good faith, however we make no representation or warranty of any kind, express or implied, regarding the accuracy, adequacy, validity, reliability, availability or completeness of any information on the Site or our mobile application. UNDER NO CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF THE SITE OR OUR MOBILE APPLICATION OR RELIANCE ON ANY INFORMATION PROVIDED ON THE SITE AND OUR MOBILE APPLICATION. YOUR USE OF THE SITE AND OUR MOBILE APPLICATION AND YOUR RELIANCE ON ANY INFORMATION ON THE SITE AND OUR MOBILE APPLICATION IS SOLELY AT YOUR OWN RISK.
-                    </p>
-                  </div>
-                </div>
-              </section>
-              <section id="section-2">
-                <div className={classes.hero}>
-                  <div >
-                    <h3>External Links Disclaimer</h3>
-                  </div>
-                  <hr />
-                  <div >
-                    <p>
-                      The Site and our mobile application may contain (or you may be sent through the Site or our mobile application) links to other websites or content belonging to or originating from third parties or links to websites and features in banners or other advertising. Such external links are not investigated, monitored, or checked for accuracy, adequacy, validity, reliability, availability or completeness by us. WE DO NOT WARRANT, ENDORSE, GUARANTEE, OR ASSUME RESPONSIBILITY FOR THE ACCURACY OR RELIABILITY OF ANY INFORMATION OFFERED BY THIRD-PARTY WEBSITES LINKED THROUGH THE SITE OR ANY WEBSITE OR FEATURE LINKED IN ANY BANNER OR OTHER ADVERTISING. WE WILL NOT BE A PARTY TO OR IN ANY WAY BE RESPONSIBLE FOR MONITORING ANY TRANSACTION BETWEEN YOU AND THIRD-PARTY PROVIDERS OF PRODUCTS OR SERVICES.
-                    </p>
-                  </div>
-                </div>
-              </section>
-              <section id="section-3">
-                <div className={classes.hero}>
-                  <div >
-                    <h3>Professional Disclaimer</h3>
-                  </div>
-                  <hr />
-                  <div >
-                    <p>
-                      The Site cannot and does not contain cryptocurrency advice. The cryptocurrency information is provided for general informational and educational purposes only and is not a substitute for professional advice. Accordingly, before taking any actions based upon such information, we encourage you to consult with the appropriate professionals. We do not provide any kind of cryptocurrency advice. THE USE OR RELIANCE OF ANY INFORMATION CONTAINED ON THIS SITE OR OUR MOBILE APPLICATION IS SOLELY AT YOUR OWN RISK.
-                    </p>
-                  </div>
-                </div>
-              </section>
-              <section id="section-4">
-                <div className={classes.hero}>
-                  <div >
-                    <h3>Affiliates Disclaimer</h3>
-                  </div>
-                  <hr />
-                  <div >
-                    <p>
-                    You may not access or use the Site for any purpose other than that for which we make the Site available. The Site may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us.
-                    </p>
-                  </div>
-                </div>
-              </section>
-              <section id="section-5">
-                <div className={classes.hero}>
-                  <div >
-                    <h3>Testimonials Disclaimer</h3>
-                  </div>
-                  <hr />
-                  <div style={{paddingBottom: 300}}>
-                    <p>
-                      The Site may contain testimonials by users of our products and/or services. These testimonials reflect the real-life experiences and opinions of such users. However, the experiences are personal to those particular users, and may not necessarily be representative of all users of our products and/or services. We do not claim, and you should not assume, that all users will have the same experiences. YOUR INDIVIDUAL RESULTS MAY VARY. 
-                    </p>
-                    <p>
-                      The testimonials on the Site are submitted in various forms such as text, audio and/or video, and are reviewed by us before being posted. They are eddited to fit the size and lenght we have space for.
-                    </p>
-                    <p>
-                      The views and opinions contained in the testimonials belong solely to the individual user and do not reflect our views and opinions. We are not affiliated with users who provide testimonials, and users are not paid or otherwise compensated for their testimonials.
-                    </p>
-                    <p>
-                      The testimonials on the Site are not intended, nor should they be construed, as claims that our products and/or services can be used to diagnose, treat, mitigate, cure, prevent or otherwise be used for any disease or medical condition. No testimonials have been clinically proven or evaluated.
-                    </p>
-                  </div>
-                </div>
-              </section>
-            </div>
-          </Col>
-        </Row>
-      </div>
+              </Col>
+            </Row>
+          </StickyContainer>
+        </div>
+      </Container>
     </React.Fragment>
   )
 }
