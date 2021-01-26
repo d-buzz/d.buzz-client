6e7d0dcde1929a6c912dc95132c3d812bb81596a
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-01-27 03:05:45 +0800

    set the text align to left on pravacy page

--- src/components/pages/PrivacyPolicy/index.js
+++ src/components/pages/PrivacyPolicy/index.js
@@ -24,7 +24,7 @@ const useStyles = createUseStyles(theme => ({
   },
   hero: {
     paddingTop: 100,
-    textAlign: 'justify',
+    textAlign: 'left',
   },
   outsideWrapper: {
     paddingTop: 50,
