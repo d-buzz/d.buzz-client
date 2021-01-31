f4c1220668ba71b865cf4c8825d3c74864f41589
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2021-01-31 16:22:13 +0800

    added login form after redirected from hive on board page

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import { Feeds, Landing } from 'components'
+import queryString from 'query-string'
+import { Feeds, Landing, LoginModal } from 'components'
 
 const Home = (props) => {
   const { user } = props
   const { is_authenticated } = user
+  const [showLogin, setShowLogin] = useState(false)
+  const [signUpConfirmation, setSignUpConfirmation] = useState(false)
+  const params = queryString.parse(props.location.search)
+  const referrer = document.referrer
+
+  const handleClickCloseLoginModal = () => {
+    setShowLogin(false)
+  }
+  useEffect(() => {
+
+    if ((params.status === 'success') && referrer === 'https://hiveonboard.com/') {
+      console.log({referrer})
+      setSignUpConfirmation(true)
+      if(!is_authenticated) {
+        setShowLogin(true)
+      } else {
+        setShowLogin(false)
+      }
+    }
+    // eslint-disable-next-line
+  }, [])
 
   return (
     <div>
+      <LoginModal
+        show={showLogin}
+        onHide={handleClickCloseLoginModal}
+        signUpConfirmation={signUpConfirmation} />
       {is_authenticated && <Feeds />}
       {!is_authenticated && <Landing />}
     </div>
