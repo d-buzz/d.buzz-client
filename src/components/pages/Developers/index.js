3893f50a75ca908dbd1ee999aabe222174b7266d
Noereyl Joy Memoria Rivera <lyereon.26@gmail.com>
2021-02-27 19:01:47 +0800

    added minimum character parameter to buzz embed button (#595) (#597)

--- src/components/pages/Developers/index.js
+++ src/components/pages/Developers/index.js
@@ -13,10 +13,10 @@ const useStyles = createUseStyles(theme => ({
     display: 'block',
     '& h2': {
       fontFamily: 'Segoe-Bold',
-      fontSize: 18, 
+      fontSize: 18,
     },
     '& p': {
-      fontSize: 14, 
+      fontSize: 14,
     },
     '& i': {
       fontSize: 14,
@@ -31,9 +31,9 @@ const useStyles = createUseStyles(theme => ({
     ...theme.font,
   },
   sideWrapper: {
-    width: '100%', 
-    paddingTop: 100, 
-    maxHeight: 700, 
+    width: '100%',
+    paddingTop: 100,
+    maxHeight: 700,
     overflow: 'auto',
     '& li': {
       listStyleType: 'none',
@@ -44,7 +44,7 @@ const useStyles = createUseStyles(theme => ({
       fontFamily: 'Segoe-Bold',
       fontSize: 14,
       textDecoration: 'none',
-      paddingLeft: 10, 
+      paddingLeft: 10,
     },
   },
   currentLink: {
@@ -62,7 +62,8 @@ const Developers = () => {
   const snippet1 = `<a class="dbuzz-share-button" href="${webIntentUrl}">Buzz</a>`
   const snippet2 = `<a class="dbuzz-share-button" href="${webIntentUrl}?text=Hello%20dbuzz">Buzz</a>`
   const snippet3 = `<a class="dbuzz-share-button" href="${webIntentUrl}" ` +
-    'data-text="Hello dbuzz" data-size="large" data-hashtags="dbuzz,hive" data-url="https://d.buzz">Buzz</a>'
+                    'data-text="Hello dbuzz" data-size="large" data-hashtags="dbuzz,hive" '+
+                    'data-url="https://d.buzz" data-min-chars="50">Buzz</a>'
   const snippet4 = `<script type="text/javascript" src="${hostUrl}/buzzWidget.js"></script>`
 
   return (
@@ -88,7 +89,7 @@ const Developers = () => {
                       scrolling="no"
                       frameBorder="0"
                       style={{ position: "static", visibility: "visible", width: "60px", height: "20px" }}
-                      src={hostUrl + "/widgets/buzz_button.html#id=dbuzz-widget-1&size=m&text=Hello world&url=" + hostUrl + "&tags=dbuzz,hive"}></iframe>
+                      src={hostUrl + "/widgets/buzz_button.html#id=dbuzz-widget-1&size=m&text=Hello world&url=" + hostUrl + "&tags=dbuzz,hive&origin_app_name=D.buzz&min_chars=50"}></iframe>
                   </div>
                   <div >
                     <h5>
@@ -134,13 +135,16 @@ const Developers = () => {
                     <br />
                     <ul>
                       <li>
-                        <p>The <b>Text</b> parameter appears pre-selected in a Buzz composer. If not set, it may be auto-populated from the webpage's title element.</p>
+                        <p>The <b>text</b> parameter appears pre-selected in a Buzz composer. If not set, it may be auto-populated from the webpage's title element.</p>
                       </li>
                       <li>
-                        <p>The <b>Url</b> parameter contains an absolute HTTP or HTTPS URL to be shared on DBuzz. If not set,  it may be auto-populated from location.href of the page.</p>
+                        <p>The <b>url</b> parameter contains an absolute HTTP or HTTPS URL to be shared on DBuzz. If not set,  it may be auto-populated from location.href of the page.</p>
                       </li>
                       <li>
-                        <p>The <b>Tags</b> parameter must be separated with a comma.  Omit a preceding "#" from each hashtag; the Buzz composer will automatically  add the proper space-separated hashtag.</p>
+                        <p>The <b>tags</b> parameter must be separated with a comma.  Omit a preceding "#" from each hashtag; the Buzz composer will automatically  add the proper space-separated hashtag.</p>
+                      </li>
+                      <li>
+                        <p>The <b>min_chars</b> parameter is the minimum number of characters required to buzz. If not set, no minimum character limit is required.</p>
                       </li>
                     </ul>
                     <br />
@@ -157,7 +161,7 @@ const Developers = () => {
                         scrolling="no"
                         frameBorder="0"
                         style={{ position: "static", visibility: "visible", width: '76px', height: '28px' }}
-                        src={hostUrl + "/widgets/buzz_button.html#id=dbuzz-widget-1&size=l&text=Hello world&url=" + hostUrl + "&tags=dbuzz,hive"}></iframe>
+                        src={hostUrl + "/widgets/buzz_button.html#id=dbuzz-widget-1&size=l&text=Hello world&url=" + hostUrl + "&tags=dbuzz,hive&origin_app_name=D.buzz&min_chars=50"}></iframe>
                     </div>
                   </div>
                 </div>
