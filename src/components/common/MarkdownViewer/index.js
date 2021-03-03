d024ae2cfe5d4c423354156e9b04d488ef7766ce
Leirariel <arielespanola143@gmail.com>
2021-03-03 17:05:51 +0800

    add lbry embed support
    Co-authored-by: Nelmin Jay Magan Anoc <monkeydev.team@gmail.com>

--- src/components/common/MarkdownViewer/index.js
+++ src/components/common/MarkdownViewer/index.js
@@ -231,6 +231,43 @@ const prepareRumbleEmbed = (content) => {
   return body
 }
 
+
+const prepareLbryEmbeds = (content) => {
+  const lbryRegex = /(?:https?:\/\/(?:(?:lbry\.tv)))/i
+  const lbry1Regex = /(?:https?:\/\/(?:(?:open\.lbry\.com)))/i
+  const lbryRegexEmbed = /(?:https?:\/\/(?:(?:lbry\.tv\/.*?\/embed\/(.*?))))/i
+  let body = content
+  
+  const links = markdownLinkExtractor(content)
+
+  links.forEach((link) => {
+    try {
+      link = link.replace(/&amp;/g, '&')
+      let match = ''
+      let id = ''
+      
+      if(link.match(lbryRegex) || link.match(lbry1Regex)){
+        const data = link.split('/')
+        match = link.match(lbryRegex) ? link.match(lbryRegex) : link.match(lbry1Regex)
+        if (data[4]) {
+          const data1 = data[4].split(':')
+          id = data1[0]
+        }
+        
+        if(link.match(lbryRegexEmbed)){
+          match = link.split('/')
+          id = match[5]
+        }
+      }
+
+      if(match){
+        body = body.replace(link, `~~~~~~.^.~~~:lbry:${id}:~~~~~~.^.~~~`)
+      }
+    } catch(error) { }
+  })
+  return body
+}
+        
 const prepareBitchuteEmbeds = (content) => {
   const bitchuteRegex = /(?:https?:\/\/(?:(?:www\.bitchute\.com\/(.*?))))/i
   const bitchuteRegexEmbed = /(?:https?:\/\/(?:(?:www\.bitchute\.com\/embed\/(.*?))))/i 
@@ -287,6 +324,10 @@ const render = (content, markdownClass, assetClass, scrollIndex, recomputeRowInd
       const url = `https://rumble.com/embed/${splitRumble[2]}`
       return <UrlVideoEmbed key={`${content}${scrollIndex}rumble`} url={url} />
     }
+  } else if(content.includes(':lbry:')){
+    const splitLbry = content.split(':')
+    const url = `https://lbry.tv/$/embed/${splitLbry[2]}`
+    return <UrlVideoEmbed key={`${url}${scrollIndex}lbry`} url={url} />
   } else if(content.includes(':bitchute:')) {
     const splitBitchute = content.split(':')
     const url = `https://www.bitchute.com/embed/${splitBitchute[2]}`
@@ -328,6 +369,8 @@ const MarkdownViewer = React.memo((props) => {
         content = prepareVimmEmbeds(content)
       } else if(link.includes('rumble.com')) {
         content = prepareRumbleEmbed(content)
+      } else if(link.includes('lbry.tv') || link.includes('open.lbry.com')) {
+        content = prepareLbryEmbeds(content)
       } else if(link.includes('www.bitchute.com')) {
         content = prepareBitchuteEmbeds(content)
       }
