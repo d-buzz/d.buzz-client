14a14a8c3bb747a312bdc8dd288894c9610f283b
Nelmin Jay Magan Anoc <monkeydev.team@gmail.com>
2021-04-15 01:13:53 +0800

    separate .env for production and development (#683)
    * add environment env
    
    * add production env
    
    * configure package json to read correct env file
    
    * remove devtool extension on production build
    
    * fix incorrect env props
    
    * fix incorrect env props
    
    * simplify conditions for compose enhancers
    
    * update ci config
    
    * update CI

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const config = {
   BUCKET: 'nathansenn-team-bucket.storage.fleek.co',
   GIPHY_API_KEY: 'ecohRlzr8FrMGrTfX8JJ4uoilgdIiZI5',
   DISABLE_MOBILE: false,
-  BRANCH: 'prod',
+  BRANCH: process.env.REACT_APP_ENV || 'dev',
 }
 
 export default config
