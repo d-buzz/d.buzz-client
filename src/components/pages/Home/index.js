93bdd3bc49035653de2a55840352f932a0d7b677
Nelmin Jay Magan Anoc <monkeydev.team@gmail.com>
2021-05-18 03:47:03 +0800

    read refereral link from d.buzz to hiveonboard (#717)

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Feeds, Landing } from 'components'
+import queryString from 'query-string'
+import { useLocation } from 'react-router-dom'
 
 const Home = (props) => {
   const { user } = props
   const { is_authenticated } = user
+  const { search } = useLocation()
+  const params = queryString.parse(search)
+
+  useEffect(() => {
+    if(params) {
+      window.location.replace(`https://hiveonboard.com/create-account?ref=${params.ref}`)
+    }
+  }, [params])
 
   return (
     <div>
