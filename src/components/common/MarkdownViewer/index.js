2fd03c3a16cd19563b11e21cedd5f724dd7dfeb6
Aashir Shaikh <hi@aashir.me>
2021-06-05 20:04:27 +0500

    add support for dapplr video embeds #147

--- src/components/common/MarkdownViewer/index.js
+++ src/components/common/MarkdownViewer/index.js
@@ -422,6 +422,37 @@ const prepareBannedEmbeds = (content) => {
   return body
 }
 
+const prepareDapplrEmbeds = (content) => {
+  const dapplrRegex = /(?:https?:\/\/(?:(?:(www\.)?cdn\.dapplr\.in\/file\/dapplr-videos\/(.*)\/(.*))))/i
+  
+  let body = content
+  
+  const links = textParser.getUrls(content)
+  
+  links.forEach((link) => {
+    try {
+      link = link.replace(/&amp;/g, '&')
+		  let match = ''
+		  let username = ''
+		  let id = ''
+  
+		  if(link.match(dapplrRegex)){
+		    const data = link.split('/')
+        match = link.match(dapplrRegex)
+        if (data[5] && data[6]) {
+          username = data[5]
+          id = data[6]
+        }
+      }
+  
+      if(match){
+        body = body.replace(link, `~~~~~~.^.~~~:dapplr:${username}/${id}:~~~~~~.^.~~~`)
+      }
+    } catch(error) { }
+  })
+  return body
+}
+
 const prepareSoundCloudEmbeds = (content) => {
   const soundcloudRegex = /^https?:\/\/(soundcloud\.com|snd\.sc)\/(.*)$/
   let body = content
@@ -599,6 +630,10 @@ const render = (content, markdownClass, assetClass, scrollIndex, recomputeRowInd
     const splitBanned = content.split(':')
     const url = `https://api.banned.video/embed/${splitBanned[2]}`
     return <UrlVideoEmbed key={`${url}${scrollIndex}banned`} url={url} />  
+  } else if(content.includes(':dapplr:')) {
+    const splitBanned = content.split(':')
+    const url = `https://cdn.dapplr.in/file/dapplr-videos/${splitBanned[2]}`
+    return <UrlVideoEmbed key={`${url}${scrollIndex}dapplr`} url={url} />  
   } else if(content.includes(':soundcloud:')) {
     const splitSoundcloud = content.split(':')
     const url = `https://soundcloud.com/${splitSoundcloud[2]}`
@@ -723,6 +758,8 @@ const MarkdownViewer = React.memo((props) => {
         content = prepareBitchuteEmbeds(content)
       } else if(link.includes('banned.video')) {
         content = prepareBannedEmbeds(content)
+      } else if(link.includes('dapplr.in')) {
+        content = prepareDapplrEmbeds(content)
       } else if(link.includes('soundcloud.com')) {
         content = prepareSoundCloudEmbeds(content)
       } else if(link.includes('facebook.com')) {
