fa7d8ca8f831f5272e1dcf376cb3370f47c421e8
Aashir Shaikh <70917922+aaashir@users.noreply.github.com>
2021-08-09 23:21:40 +0500

    add more menu (#834)

--- src/components/common/MoreMenu/index.js
+++ src/components/common/MoreMenu/index.js
@@ -1,41 +1,56 @@
-// import React, { useState, useEffect } from 'react'
-// import Menu from '@material-ui/core/Menu'
-// import MenuItem from '@material-ui/core/MenuItem'
+import React, { useState, useEffect } from 'react'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
-// const MoreMenu = (props) => {
-//   const {
-//     open,
-//     onClose,
-//     anchor,
-//     items,
-//     className,
-//   } = props
+const MoreMenu = (props) => {
+  const {
+    open,
+    onClose,
+    anchor,
+    items,
+    className,
+    themeModal,
+    switchUserModal,
+  } = props
 
-//   const [callFunc, setcallFunc] = useState()
+  const [callFunc, setcallFunc] = useState()
 
-//   const handleMenuClosing = (onClick) => () => {
-//     onClose()
-//     setcallFunc(onClick)
-//   }
+  const setOverflow = (value) => {
+    document.querySelector('body').style.overflowY = value
+  }
+
+  const handleMenuClosing = (onClick) => () => {
+    onClose()
+    setcallFunc(onClick)
+    setOverflow('scroll')
+  }
  
-//   useEffect(() => {
-//     callFunc
-//   }, [open])
+  useEffect(() => {
+    console.log(open, themeModal, switchUserModal)
+    setcallFunc(callFunc)
+    if(!open && !themeModal && !switchUserModal){
+      setOverflow('scroll')
+      // eslint-disable-next-line
+    } else if(!open && themeModal || switchUserModal){
+      setOverflow('hidden')
+    }
+    // eslint-disable-next-line
+  }, [open, themeModal, switchUserModal])
 
-//   return (
-//     <Menu
-//       style={{ zIndex: 3500 }}
-//       anchorEl={() => anchor.current}
-//       open={open}
-//       onClose={onClose}
-//       className={className}
-//       transformOrigin={{vertical: 'bottom', horizontal: 'top'}}
-//     >
-//       {items.map(({onClick, text}) => (
-//         <MenuItem onClick={handleMenuClosing(onClick)}>{text}</MenuItem>
-//       ))}
-//     </Menu>
-//   )
-// }
+  return (
+    <Menu
+      style={{ zIndex: 3500 }}
+      anchorEl={() => anchor.current}
+      open={open}
+      onClose={onClose}
+      className={className}
+      transformOrigin={{vertical: 'bottom', horizontal: 'top'}}
+    >
+      {items.map(({onClick, text}) => (
+        <MenuItem onClick={handleMenuClosing(onClick)}>{text}</MenuItem>
+      ))}
+    </Menu>
+  )
+}
 
-// export default MoreMenu
+export default MoreMenu
