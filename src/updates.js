8d4cecdf83f20a7c0fc00ca999ef6638c1495cfd
Aashir Shaikh <hi@aashir.me>
2021-10-02 23:13:44 +0500

    updates-and-bug-fixes

--- src/updates.js
+++ src/updates.js
@@ -2,21 +2,18 @@ const updates = {
   status: true,
 
   changes: `
-  ### **DBuzz's New Image Upload Grid 🖼**
-  ###### <br /> Learn more about it here 👇 [](https://peakd.com/hive-139531/@dbuzz/weekly-development-updates-for-dbuzz-today-more-with-images-and-other-updates)
+  ### **A New Settings Page ⚙**
+  ### **Customizable Buzz Title 😊🖼**
+  ###### <br /> Learn more about these here 👇 [](https://peakd.com/hive-139531/@dbuzz/weekly-development-updates-for-dbuzz-today-add-a-title-and-a-new-settings-page)
 `,
 
   improvements: `
-  - Better username, url, hashtag and crypto ticker support.
-  - Updated profile page with username support in the bio.
-  - Added 404 page support for the blank pages including invalid profiles and buzzes.
+  - Addional sharing option to copy buzz link to clipboard.
   `,
   
   fixes: `
-  - Fixed few bugs in the threads feature.
-  - Fixed bugs in the markdown renderer.
-  - Fixed emoji placement bug in the comments and update buzz modal.
-  - Other minor bug fixes to improve DBuzz even better.
+  - Fixed buzz posting error.
+  - Other minor bug fixes.
   `,
 }
 
