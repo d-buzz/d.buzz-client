6cd8535f466bf6b3a4623e894cdb1552705db52b
Aashir Shaikh <70917922+aaashir@users.noreply.github.com>
2023-01-27 22:49:42 +0500

    bug fixes 3.49.1 (#998)

--- src/components/pages/Content/index.js
+++ src/components/pages/Content/index.js
@@ -233,6 +233,7 @@ const Content = (props) => {
   const [openVoteList, setOpenVoteList] = useState(false)
   const popoverAnchor = useRef(null)
   const history = useHistory()
+  const [contentLength, setContentLength] = useState(0)
   const [overhead, setOverhead] = useState(0)
   const [invalidBuzz, setInvalidBuzz] = useState(false)
   const [addToPocketModal, setAddToPocketModal] = useState(false)
@@ -298,15 +299,19 @@ const Content = (props) => {
     // eslint-disable-next-line
   }, [content.body])
 
-  // useEffect(() => {
-  //   if(overhead && content.body) {
-  //     if(content.body) {
-  //       console.log(content.body);
-  //       console.log(overhead)
-  //       console.log(content.body.length - overhead);
-  //     }
-  //   }
-  // }, [content, overhead])
+  useEffect(() => {
+    if(overhead && content.body) {
+      setContentLength(stripHtml(content.body).length - overhead)
+      // const fullContent = stripHtml(content.body)
+
+      // if(content.body) {
+      //   console.log(content.body);
+      //   console.log(fullContent.length);
+      //   console.log(overhead)
+      //   console.log(fullContent.length - overhead);
+      // }
+    }
+  }, [content, overhead])
 
   useEffect(() => {
     checkHasUpdateAuthorityRequest(username)
@@ -526,6 +531,9 @@ const Content = (props) => {
     setSelectedAddToPocketBuzz(null)
   }
 
+  useEffect(() => {
+  }, [originalContent])
+
   return (
     <React.Fragment>
       <Helmet>
@@ -539,7 +547,7 @@ const Content = (props) => {
           <div className={classes.wrapper}>
             <br />
             <React.Fragment>
-              {depth !== 0 && parent_author !== null && !(content.body.length - overhead > 280) && (
+              {depth !== 0 && parent_author !== null && (contentLength > 280) && (
                 <Row>
                   <Col>
                     <div className={classes.context}>
@@ -592,7 +600,7 @@ const Content = (props) => {
                 <Renderer content={originalContent} minifyAssets={false} />
               </div>
               {/* <PostTags meta={meta} /> */}
-              {(`${stripHtml(content.body)}`.length - overhead > 280) && (
+              {(contentLength > 280) && (
                 <Row>
                   <Col>
                     <div className={classes.context}>
