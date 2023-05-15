035574a22ebef441bba3170899800217662f181c
Aashir Shaikh <hi@aashir.me>
2023-05-15 10:41:03 +0500

    add vote for proposal banner

--- src/components/pages/Trending/index.js
+++ src/components/pages/Trending/index.js
@@ -24,6 +24,42 @@ import {
 import { anchorTop } from 'services/helper'
 import { InfiniteList, HelmetGenerator } from 'components'
 import { clearScrollIndex, clearRefreshRouteStatus } from 'store/interface/actions'
+import { createUseStyles } from 'react-jss'
+import { isMobile } from 'react-device-detect'
+
+const useStyles = createUseStyles(theme => ({
+  opensourceWrapper: {
+    padding: '25px 0px 25px 0px',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#e6ecf0',
+
+    '& .title': {
+      width: 'fit-content',
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+    
+    '& .button': {
+      marginTop: 15,
+      borderRadius: 15,
+      width: 'fit-content',
+      padding: '5px 15px 5px 15px',
+      fontSize: 18,
+      fontWeight: 'bold',
+      background: '#E61C34',
+      color: '#FFFFFF',
+      cursor: 'pointer',
+      userSelect: 'none',
+
+      '&:hover': {
+        opacity: 0.85,
+      }
+    },
+  },
+}))
 
 const Trending = (props) => {
   const {
@@ -51,6 +87,8 @@ const Trending = (props) => {
     refreshRouteStatus,
     clearRefreshRouteStatus,
   } = props
+
+  const classes = useStyles()
 
   useEffect(() => {
     setPageFrom('trending')
@@ -91,9 +129,18 @@ const Trending = (props) => {
     // eslint-disable-next-line
   }, [last])
 
+  const handleReirectToProposal = () => {
+    return window.location = 'https://vote.d.buzz'
+  }
+
   return (
     <React.Fragment>
       <HelmetGenerator page='Trending' />
+      <div className={classes.opensourceWrapper}>
+        
+        {isMobile ? <span className='title'>Support & Open Source : D.Buzz</span> : <span className='title'>Help us OPEN SOURCE & continue : DBUZZ</span>}
+        <span className='button' onClick={handleReirectToProposal}>Vote for DBuzz Proposal</span>
+      </div>
       <InfiniteList unguardedLinks={unguardedLinks} loading={loading} items={items} onScroll={loadMorePosts} />
     </React.Fragment>
   )
