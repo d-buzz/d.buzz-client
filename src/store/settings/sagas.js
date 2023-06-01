0d97e72a350c0599ab907058d11332771a016ade
Aashir Shaikh <hi@aashir.me>
2023-06-02 01:55:16 +0500

    hotfix: attempt update notification

--- src/store/settings/sagas.js
+++ src/store/settings/sagas.js
@@ -75,11 +75,6 @@ function* checkVersionRequest(meta) {
 
   const latest = (VERSION === remote[BRANCH])
 
-  console.log('VERSION', VERSION)
-  console.log('BRANCH', BRANCH)
-  
-  console.log('latest', latest)
-
   if(!latest) {
     yield call([localStorage, localStorage.setItem], 'version', JSON.stringify(remote))
   }
