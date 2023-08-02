8633dfa3a799a4024c6852b2948ad34d467b87cb
Kenneth Kempis <kennethroykempis@gmail.com>
2023-08-02 16:16:32 +0800

    modify icon used as thumbnail

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
         <Helmet>
           <meta property="og:title" content="D.Buzz" />
           <meta property="og:description" content="D.Buzz | Micro-blogging for HIVE" />
-          <meta property="og:image" content="https://d.buzz/dbuzz-icon.svg" />
+          <meta property="og:image" content="https://d.buzz/dbuzz.svg" />
           <meta property="title" content="D.Buzz" />
           <meta property="description" content="D.Buzz | Micro-blogging for HIVE" />
           <meta property="image" content="https://d.buzz/dbuzz-icon.svg" />
