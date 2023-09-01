8fd1513c4531fa0fe67ad38a82407a086d5e2a7e
Aashir Shaikh <hi@aashir.me>
2023-09-01 08:38:16 +0500

    fix: styles for custom themes

--- src/components/common/ImageCropper/index.js
+++ src/components/common/ImageCropper/index.js
@@ -1,9 +1,86 @@
 import React, { useState, useRef } from 'react'
+import {createUseStyles} from 'react-jss'
 import Cropper from 'react-cropper'
 import 'cropperjs/dist/cropper.css'  // Import cropper styles
 import { Modal, Button } from 'react-bootstrap'
+import classNames from 'classnames'
+
+const useStyles = createUseStyles(theme => ({
+  modal: {
+    width: 600,
+    '& div.modal-content': {
+      margin: '0 auto',
+      backgroundColor: theme.background.primary,
+      width: 600,
+      borderRadius: '20px 20px !important',
+      border: 'none',
+      boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+    },
+    '@media (max-width: 900px)': {
+      width: '97% !important',
+      '& div.modal-content': {
+        margin: '0 auto',
+        width: '97% !important',
+      },
+    },
+  },
+  modalBody: {
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  modalTitle: {
+    color: theme.font.color,
+  },
+  button: {
+    padding: 8,
+    width: '100%',
+    flex: 0.5,
+    textAlign: 'center',
+    borderRadius: 35,
+    color: '#000000',
+    fontSize: '1.2em',
+    fontWeight: '600',
+    userSelect: 'none',
+    cursor: 'pointer',
+    outline: 'none !important',
+    border: 'none !important',
+    boxShadow: 'none !important',
+  },
+  doneButton: {
+    background: '#E61C34',
+    color: '#ffffff',
+
+    '&:hover': {
+      background: '#B71C1C',
+    },
+
+    '&:active': {
+      background: '#B71C1C !important',
+    },
+  },
+  cancelButton: {
+    background: '#F5F8FA',
+    marginRight: 8,
+    color: '#000000 !important',
+    
+    '&:hover': {
+      background: '#EDF0F2',
+    },
+    
+    '&:active': {
+      background: '#EDF0F2 !important',
+    },
+
+    '&:disabled': {
+      '&:hover': {
+        background: '#F5F8FA',
+      },
+    },
+  },
+}))
 
 const ImageCropper = ({ isOpen, onClose, src, onCropComplete }) => {
+  const classes = useStyles()
   const [cropData, setCropData] = useState("#")
   const cropperRef = useRef(null)
 
@@ -23,11 +100,11 @@ const ImageCropper = ({ isOpen, onClose, src, onCropComplete }) => {
 
 
   return (
-    <Modal show={isOpen} onHide={onClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Crop your image</Modal.Title>
+    <Modal show={isOpen} onHide={onClose} dialogClassName={classes.modal}>
+      <Modal.Header>
+        <Modal.Title className={classes.modalTitle}>Crop profile image</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className={classes.modalBody}>
         <Cropper
           ref={cropperRef}
           src={src}
@@ -37,8 +114,8 @@ const ImageCropper = ({ isOpen, onClose, src, onCropComplete }) => {
         />
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={onClose}>Close</Button>
-        <Button variant="primary" onClick={getCropData}>Crop Image</Button>
+        <Button variant="secondary" onClick={onClose} className={classNames(classes.button, classes.cancelButton)}>Close</Button>
+        <Button variant="primary" onClick={getCropData} className={classNames(classes.button, classes.doneButton)}>Crop Image</Button>
       </Modal.Footer>
     </Modal>
   )
