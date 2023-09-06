e995d39c9f336eeb857a501be822eacf9806d379
Aashir Shaikh <hi@aashir.me>
2023-09-07 04:52:47 +0500

    fix: theme bugs

--- src/services/theme.js
+++ src/services/theme.js
@@ -363,189 +363,187 @@ const light = {
   },
 }
 
-const gray = {
-  background: {
-    primary: '#202225',
-    secondary: '#192734',
-  },
-  coverColor: '#425364',
-  notification: {
-    backgroundColor: '#272b2d !important',
-  },
-  messageColor: {
-    color: 'lightGreen !important',
-  },
-  unread: {
-    backgroundColor: '#2a3439',
-    '& label': {
-      color: 'white',
-    },
-    '&:hover': {
-      backgroundColor: '#354147 !important',
-      '& label': {
-        color: '#f83541 !important',
-      },
-    },
-  },
-  context: {
-    view: {
-      backgroundColor: '#282a2d',
-      color: 'rgb(255, 255, 255)',
-    },
-  },
-  nav: {
-    background: '#282a2d',
-  },
-  dialog: {
-    user: {
-      boxShadow: 'rgba(136, 153, 166, 0.2) 0px 0px 15px, rgba(136, 153, 166, 0.15) 0px 0px 3px 1px !important',
-      color: 'rgb(255 255 255 / 88%) !important',
-      '& a': {
-        color: 'rgb(255, 255, 255)',
-      },
-    },
-  },
-  icon: {
-    '& svg': {
-      '& path': {
-        stroke: 'white',
-        fill: 'white',
-      },
-    },
-  },
-  iconButton: {
-    hover: {
-      '&:hover': {
-        backgroundColor: '#36393fed !important',
-      },
-    },
-  },
-  font: {
-    color: 'rgb(255, 255, 255) !important',
-  },
-  textIcon : {
-    color: 'rgb(136, 153, 166)',
-  },
-  border: {
-    primary: '1px solid #2f3136',
-    thick: '10px solid #2f3136',
-    background: 'rgb(56, 68, 77)',
-  },
-  textArea: {
-    // backgroundColor: '#202225',
-    color: 'white',
-    borderBottom: '1px solid rgb(56, 68, 77)',
-    '&::placeholder': {
-      color: 'white',
-    },
-  },
-  search: {
-    background: {
-      backgroundColor: '#202225',
-    },
-  },
-  navbar: {
-    icon: {
-      '& svg': {
-        '& path': {
-          stroke: 'white',
-          fill: 'white',
-        },
-      },
-    },
-  },
-  preview: {
-    title: {
-      color: 'white',
-    },
-    hover: {
-      color: '#282a2d',
-    },
-  },
-  markdown: {
-    paragraph: {
-      color: 'rgb(255 255 255 / 88%) !important',
-    },
-  },
-  postList: {
-    hover: {
-      backgroundColor: '#393c436b',
-    },
-  },
-  left: {
-    sidebar: {
-      items: {
-        color: 'white',
-        hover: {
-          backgroundColor: '#36393f80',
-        },
-        icons: {
-          '& svg': {
-            '& path': {
-              strokeWidth: 1,
-              stroke: 'black !important',
-              fill: 'white !important',
-            },
-          },
-        },
-      },
-      bottom: {
-        wrapper: {
-          backgroundColor: '#36393f80 !important',
-          '&:hover': {
-            backgroundColor: '#36393fed !important',
-          },
-        },
-        wrapperMinify: {
-          '& svg': {
-            '& path': {
-              stroke: 'white',
-              fill: 'white',
-            },
-          },
-        },
-      },
-      logout: {
-        label: {
-          color: 'white',
-        },
-        username: {
-          color: 'rgb(136, 153, 166)',
-        },
-        icon: {
-          '& svg': {
-            '& path': {
-              stroke: 'white !important',
-              fill: 'white !important',
-            },
-          },
-        },
-      },
-    },
-  },
-  right: {
-    list: {
-      background: '#282a2d',
-      hover: {
-        backgroundColor: '#36393fed',
-      },
-    },
-  },
-  skeleton: {
-    color: '#2f3136',
-    highlight: '#36393fed',
-  },
-  seeMoreReplies: {
-    background: '#282A2D',
-    color: '#ffffff',
-  },
-}
+// const gray = {
+//   background: {
+//     primary: '#202225',
+//     secondary: '#192734',
+//   },
+//   coverColor: '#425364',
+//   notification: {
+//     backgroundColor: '#272b2d !important',
+//   },
+//   messageColor: {
+//     color: 'lightGreen !important',
+//   },
+//   unread: {
+//     backgroundColor: '#2a3439',
+//     '& label': {
+//       color: 'white',
+//     },
+//     '&:hover': {
+//       backgroundColor: '#354147 !important',
+//       '& label': {
+//         color: '#f83541 !important',
+//       },
+//     },
+//   },
+//   context: {
+//     view: {
+//       backgroundColor: '#282a2d',
+//       color: 'rgb(255, 255, 255)',
+//     },
+//   },
+//   nav: {
+//     background: '#282a2d',
+//   },
+//   dialog: {
+//     user: {
+//       boxShadow: 'rgba(136, 153, 166, 0.2) 0px 0px 15px, rgba(136, 153, 166, 0.15) 0px 0px 3px 1px !important',
+//       color: 'rgb(255 255 255 / 88%) !important',
+//       '& a': {
+//         color: 'rgb(255, 255, 255)',
+//       },
+//     },
+//   },
+//   icon: {
+//     '& svg': {
+//       '& path': {
+//         stroke: 'white',
+//         fill: 'white',
+//       },
+//     },
+//   },
+//   iconButton: {
+//     hover: {
+//       '&:hover': {
+//         backgroundColor: '#36393fed !important',
+//       },
+//     },
+//   },
+//   font: {
+//     color: 'rgb(255, 255, 255) !important',
+//   },
+//   textIcon : {
+//     color: 'rgb(136, 153, 166)',
+//   },
+//   border: {
+//     primary: '1px solid #2f3136',
+//     thick: '10px solid #2f3136',
+//     background: 'rgb(56, 68, 77)',
+//   },
+//   textArea: {
+//     // backgroundColor: '#202225',
+//     color: 'white',
+//     borderBottom: '1px solid rgb(56, 68, 77)',
+//     '&::placeholder': {
+//       color: 'white',
+//     },
+//   },
+//   search: {
+//     background: {
+//       backgroundColor: '#202225',
+//     },
+//   },
+//   navbar: {
+//     icon: {
+//       '& svg': {
+//         '& path': {
+//           stroke: 'white',
+//           fill: 'white',
+//         },
+//       },
+//     },
+//   },
+//   preview: {
+//     title: {
+//       color: 'white',
+//     },
+//     hover: {
+//       color: '#282a2d',
+//     },
+//   },
+//   markdown: {
+//     paragraph: {
+//       color: 'rgb(255 255 255 / 88%) !important',
+//     },
+//   },
+//   postList: {
+//     hover: {
+//       backgroundColor: '#393c436b',
+//     },
+//   },
+//   left: {
+//     sidebar: {
+//       items: {
+//         color: 'white',
+//         hover: {
+//           backgroundColor: '#36393f80',
+//         },
+//         icons: {
+//           '& svg': {
+//             '& path': {
+//               strokeWidth: 1,
+//               stroke: 'black !important',
+//               fill: 'white !important',
+//             },
+//           },
+//         },
+//       },
+//       bottom: {
+//         wrapper: {
+//           backgroundColor: '#36393f80 !important',
+//           '&:hover': {
+//             backgroundColor: '#36393fed !important',
+//           },
+//         },
+//         wrapperMinify: {
+//           '& svg': {
+//             '& path': {
+//               stroke: 'white',
+//               fill: 'white',
+//             },
+//           },
+//         },
+//       },
+//       logout: {
+//         label: {
+//           color: 'white',
+//         },
+//         username: {
+//           color: 'rgb(136, 153, 166)',
+//         },
+//         icon: {
+//           '& svg': {
+//             '& path': {
+//               stroke: 'white !important',
+//               fill: 'white !important',
+//             },
+//           },
+//         },
+//       },
+//     },
+//   },
+//   right: {
+//     list: {
+//       background: '#282a2d',
+//       hover: {
+//         backgroundColor: '#36393fed',
+//       },
+//     },
+//   },
+//   skeleton: {
+//     color: '#2f3136',
+//     highlight: '#36393fed',
+//   },
+//   seeMoreReplies: {
+//     background: '#282A2D',
+//     color: '#ffffff',
+//   },
+// }
 
 export const getTheme = (mode) => {
-  if(mode === 'night') {
+  if(mode === 'night' || mode === 'gray') {
     return night
-  } else if(mode === 'gray') {
-    return gray
   } else {
     return light
   }
