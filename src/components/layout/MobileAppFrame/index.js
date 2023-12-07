23fcf488690da6668a6f1b6c74066ec7f89f6623
Aashir Shaikh <hi@aashir.me>
2023-12-08 02:02:27 +0500

    fixed runtime warnings

--- src/components/layout/MobileAppFrame/index.js
+++ src/components/layout/MobileAppFrame/index.js
@@ -40,7 +40,6 @@ import {
   SwitchUserModal,
   LoginModal,
   SearchField,
-  MoreMenu,
 } from 'components'
 import { useLocation } from 'react-router-dom'
 import Fab from '@material-ui/core/Fab'
