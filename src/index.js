63fadac6af86206db00b0b7bc8ee793de259313f
Gerard Baluyot <71457640+gacbaluyot@users.noreply.github.com>
2024-02-21 02:27:24 +0800

    added sentry tracing

--- src/index.js
+++ src/index.js
@@ -11,14 +11,34 @@ import initReactFastclick from 'react-fastclick'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './override.css'
 import HttpsRedirect from 'react-https-redirect'
+// src/sentry.js
+import * as Sentry from '@sentry/react'
+import {Profiler} from "@sentry/react"
 
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  enabled: process.env.REACT_APP_SENTRY_DSN !== undefined,
+  tracesSampleRate:  1.0,
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 initReactFastclick()
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <HttpsRedirect>
-        <App />
+        <Profiler id="App" tracesSampleRate={1.0}>
+          <App />
+        </Profiler>
       </HttpsRedirect>
     </Router>
   </Provider>,
