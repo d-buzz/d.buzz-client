3fc9e35c05e4bd28db57a9e4db233d4525e4e1be
Nelmin Jay Magan Anoc <monkeydev.team@gmail.com>
2024-02-22 03:08:02 +0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -21,8 +21,8 @@ clientsClaim()
 // even if you decide not to use precaching. See https://cra.link/PWA
 precacheAndRoute(self.__WB_MANIFEST)
 
-const currentVersion = parseInt(self.registration.active.scriptURL.split('/').pop());
-const newVersion = parseInt(self.registration.waiting?.scriptURL.split('/').pop() ?? 0);
+const currentVersion = parseInt(self.registration.active.scriptURL.split('/').pop())
+const newVersion = parseInt(self.registration.waiting?.scriptURL.split('/').pop() ?? 0)
 
 console.log("currentVersion >>", currentVersion)
 console.log("newVersion >>", newVersion)
