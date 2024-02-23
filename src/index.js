17703adf52457c50bea4fd323a99fa4b4817f79a
Gerard Baluyot <71457640+gacbaluyot@users.noreply.github.com>
2024-02-23 09:47:47 +0800

    increment version

--- src/index.js
+++ src/index.js
@@ -25,14 +25,29 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 serviceWorker.register({
-  onUpdate: registration => {
-    alert('New version available!  Ready to update?')
+  onUpdate: async registration => {
+    // We want to run this code only if we detect a new service worker is
+    // waiting to be activated.
+    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
     if (registration && registration.waiting) {
+      await registration.unregister()
+      // Makes Workbox call skipWaiting()
       registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+      // Once the service worker is unregistered, we can reload the page to let
+      // the browser download a fresh copy of our app (invalidating the cache)
+      window.location.reload()
     }
-    window.location.reload()
   },
 })
+// serviceWorker.register({
+//   onUpdate: registration => {
+//     alert('New version available!  Ready to update?')
+//     if (registration && registration.waiting) {
+//       registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+//     }
+//     window.location.reload()
+//   },
+// })
 // serviceWorker.register({
 //   onUpdate: async (registration) => {
 //     // We want to run this code only if we detect a new service worker is
