cfeb2d7846ad9051dbcfe81c532e05aebbdbd531
stinkymonkeyph <monkeydev.team@gmail.com>
2024-02-23 21:45:36 +0800

    test solution

--- src/App.js
+++ src/App.js
@@ -1,69 +1,91 @@
-import React, { useEffect } from 'react'
-import routes from './routes'
-import { withRouter } from 'react-router'
-import { Init, AuthGuard, ThemeLoader } from 'components'
-import { renderRoutes } from 'react-router-config'
-import { LastLocationProvider } from 'react-router-last-location'
-import { createUseStyles } from 'react-jss'
-import { Helmet } from 'react-helmet'
-import { redirectOldLinks } from 'services/helper'
-import { useLocation } from 'react-router-dom'
-const TwitterEmbedAPI = React.lazy(() => import('components/pages/TwitterEmbedAPI'))
+import React, { useEffect } from "react";
+import routes from "./routes";
+import { withRouter } from "react-router";
+import { Init, AuthGuard, ThemeLoader } from "components";
+import { renderRoutes } from "react-router-config";
+import { LastLocationProvider } from "react-router-last-location";
+import { createUseStyles } from "react-jss";
+import { Helmet } from "react-helmet";
+import { redirectOldLinks } from "services/helper";
+import { useLocation } from "react-router-dom";
+const TwitterEmbedAPI = React.lazy(
+  () => import("components/pages/TwitterEmbedAPI"),
+);
 
-const useStyles = createUseStyles(theme => ({
-  wrapper: {
-    overflow: 'hidden !important',
-    backgroundColor: theme.background.primary,
-  },
-}))
+function checkForUpdate() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.addEventListener(
+      "controllerchange",
+      function (event) {
+        window.location.reload();
+      },
+    );
 
-const AppWrapper = ({ children }) => {
-  const classes = useStyles()
-  return (
-    <div className={classes.wrapper}>
-      {children}
-    </div>
-  )
+    // Send a message to the service worker
+    navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
+  }
 }
 
+const useStyles = createUseStyles((theme) => ({
+  wrapper: {
+    overflow: "hidden !important",
+    backgroundColor: theme.background.primary,
+  },
+}));
+
+const AppWrapper = ({ children }) => {
+  const classes = useStyles();
+  return <div className={classes.wrapper}>{children}</div>;
+};
+
 const App = () => {
-  const { pathname } = useLocation()
-  const twitterEmbedRoutes = pathname.match(/^\/twitterEmbed/)
-  const currentSiteUrl = window.location.protocol + '//' + window.location.host
+  const { pathname } = useLocation();
+  const twitterEmbedRoutes = pathname.match(/^\/twitterEmbed/);
+  const currentSiteUrl = window.location.protocol + "//" + window.location.host;
+
+  useEffect(() => {
+    checkForUpdate();
+  }, []);
 
   useEffect(() => {
     // redirect old links to the new ones
-    redirectOldLinks()
-  }, [])
-  
+    redirectOldLinks();
+  }, []);
+
   return (
     <React.Fragment>
       <React.Suspense fallback={<span> </span>}>
         <Helmet>
           <link rel="canonical" href={currentSiteUrl} />
           <meta property="og:title" content="D.Buzz" />
-          <meta property="og:description" content="D.Buzz | Micro-blogging for HIVE | Connect with thought leaders and like-minded individuals on d.buzz. Explore trending topics, share your insights, and join the community today." />
+          <meta
+            property="og:description"
+            content="D.Buzz | Micro-blogging for HIVE | Connect with thought leaders and like-minded individuals on d.buzz. Explore trending topics, share your insights, and join the community today."
+          />
           <meta property="og:image" content="https://d.buzz/dbuzz.svg" />
           <meta property="title" content="D.Buzz" />
-          <meta property="description" content="D.Buzz | Micro-blogging for HIVE | Connect with thought leaders and like-minded individuals on d.buzz. Explore trending topics, share your insights, and join the community today." />
+          <meta
+            property="description"
+            content="D.Buzz | Micro-blogging for HIVE | Connect with thought leaders and like-minded individuals on d.buzz. Explore trending topics, share your insights, and join the community today."
+          />
           <meta property="image" content="https://d.buzz/dbuzz-icon.svg" />
         </Helmet>
         <LastLocationProvider>
           <ThemeLoader>
-            {!twitterEmbedRoutes ?
+            {!twitterEmbedRoutes ? (
               <Init>
                 <AuthGuard>
-                  <AppWrapper>
-                    {renderRoutes(routes)}
-                  </AppWrapper>
+                  <AppWrapper>{renderRoutes(routes)}</AppWrapper>
                 </AuthGuard>
-              </Init> :
-              <TwitterEmbedAPI />}
+              </Init>
+            ) : (
+              <TwitterEmbedAPI />
+            )}
           </ThemeLoader>
         </LastLocationProvider>
       </React.Suspense>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default withRouter(App)
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/'
-import App from './App'
-import store from 'store/store'
-import * as serviceWorker from './serviceWorker'
-import { Provider } from 'react-redux'
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom'
-import initReactFastclick from 'react-fastclick'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './override.css'
-import HttpsRedirect from 'react-https-redirect'
+import React from "react";
+import ReactDOM from "react-dom/";
+import App from "./App";
+import store from "store/store";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import initReactFastclick from "react-fastclick";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./override.css";
+import HttpsRedirect from "react-https-redirect";
 
-initReactFastclick()
+initReactFastclick();
 
 ReactDOM.render(
   <Provider store={store}>
@@ -22,25 +20,25 @@ ReactDOM.render(
       </HttpsRedirect>
     </Router>
   </Provider>,
-  document.getElementById('root'),
-)
-serviceWorker.register({
-  onUpdate: async (registration) => {
-    // We want to run this code only if we detect a new service worker is
-    // waiting to be activated.
-    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
-    if (registration && registration.waiting) {
-      await registration.unregister()
-      registration.waiting.addEventListener('statechange', (event) => {
-        const sw = event.target
-        if (sw && sw.state === 'activated') {
-          // Once the service worker is unregistered, we can reload the page to let
-          // the browser download a fresh copy of our app (invalidating the cache)
-          window.location.reload()
-        }
-      })
-      // Makes Workbox call skipWaiting() that will trigger upper listener
-      registration.waiting.postMessage({ type: 'SKIP_WAITING' })
-    }
-  },
-})
+  document.getElementById("root"),
+);
+//serviceWorker.register({
+//  onUpdate: async (registration) => {
+//    // We want to run this code only if we detect a new service worker is
+//    // waiting to be activated.
+//    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+//    if (registration && registration.waiting) {
+//      await registration.unregister()
+//      registration.waiting.addEventListener('statechange', (event) => {
+//        const sw = event.target
+//        if (sw && sw.state === 'activated') {
+//          // Once the service worker is unregistered, we can reload the page to let
+//          // the browser download a fresh copy of our app (invalidating the cache)
+//          window.location.reload()
+//        }
+//      })
+//      // Makes Workbox call skipWaiting() that will trigger upper listener
+//      registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+//    }
+//  },
+//})
