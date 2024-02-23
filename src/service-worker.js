ec7446ba79d72861d96e85c722fb456bafc53763
stinkymonkeyph <monkeydev.team@gmail.com>
2024-02-24 00:16:35 +0800

    test service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -13,6 +13,8 @@ import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching"
 import { registerRoute } from "workbox-routing"
 import { StaleWhileRevalidate } from "workbox-strategies"
 
+self.skipWaiting()
+
 clientsClaim()
 
 //self.skipWaiting()
