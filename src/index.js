7afbfadfb8d327d3b83dc273c6254944b5a7c4f7
stinkymonkeyph <monkeydev.team@gmail.com>
2024-02-24 01:55:59 +0800

    test service worker

--- src/index.js
+++ src/index.js
@@ -47,12 +47,15 @@ ReactDOM.render(
 //
 serviceWorker.register({
   onUpdate: (registration) => {
-    registration.unregister().then(() => {
-      window.location.reload()
-    })
-  },
-  onSuccess: (registration) => {
-    console.info("service worker on success state")
-    console.log(registration)
+    const waitingServiceWorker = registration.waiting
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener("statechange", (event) => {
+        if (event.target.state === "activated") {
+          window.location.reload()
+        }
+      })
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" })
+    }
   },
 })
